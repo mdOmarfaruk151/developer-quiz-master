4f180da4761edd19a35a2d5e799b9d2834618a23
mdOmarfaruk151 <mdomar28151@gmile.com>
2022-10-31 02:03:58 +0600

    readded charts useing Axios API

--- src/Componentes/Statistics/Statistics.js
+++ src/Componentes/Statistics/Statistics.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "../images/Chart-Data.png";
+import axios from "axios";
 import {
   LineChart,
   Line,
@@ -14,69 +15,63 @@ import {
 } from "recharts";
 
 const Statistics = () => {
-  // ! data !!
-  const data = [
-    {
-      name: "React",
-      Total: 8,
-    },
-    {
-      name: "JavaScript",
-      Total: 9,
-    },
-    {
-      name: "CSS",
-      Total: 8,
-    },
-    {
-      name: "Git",
-      Total: 11,
-    },
-  ];
+  const [charts, setCharts] = useState([]);
 
+  axios.get("https://openapi.programming-hero.com/api/quiz").then((data) => {
+    const chartsLoaded = data.data.data;
+    //   console.log(chartsLoaded);
+    const chartData = chartsLoaded.map((chart) => {
+      const singleChart = {
+        Name: chart.name,
+        Total: chart.total,
+      };
+      return singleChart;
+    });
+    // console.log(chartData);
+    setCharts(chartData);
+  });
+  //   // ! data !!
   return (
     <div>
       <img className="mx-auto w-96 mt-0" src={Chart} alt="" />
 
- <div className="flex mb-20 justify-center ">
-         {/* //! Line Chart */}
-         <div className="border-solid border-2 border-sky-500 font-sans mr-2 rounded-xl bg-sky-50 font-bold  ">
-         <h1 className="text-center mb-3 mt-2 ">Line Chart</h1>
+      <div className="flex mb-20 justify-center ">
+        {/* //! Line Chart */}
+        <div className="border-solid border-2 border-sky-500 font-sans mr-2 rounded-xl bg-sky-50 font-bold  ">
+          <h1 className="text-center mb-3 mt-2 ">Line Chart</h1>
 
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Total" stroke="#82ca9d" />
-        </LineChart>
+          <LineChart
+            width={500}
+            height={300}
+            data={charts}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="Total" stroke="#82ca9d" />
+          </LineChart>
+        </div>
+
+        {/* //! Bar Chart */}
+        <div className="border-solid border-2 border-sky-500 font-sans ml-2 rounded-xl bg-sky-50 font-bold ">
+          <h1 className="text-center mb-3 mt-2 ">Bar Chart</h1>
+          <BarChart width={500} height={300} data={charts}>
+            <Bar dataKey="Total" fill="#8884d8" />
+            <XAxis dataKey="Name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+          </BarChart>
+        </div>
       </div>
-
-      {/* //! Bar Chart */}
-      <div className="border-solid border-2 border-sky-500 font-sans ml-2 rounded-xl bg-sky-50 font-bold ">
-        <h1 className="text-center mb-3 mt-2 ">Bar Chart</h1>
-        <BarChart width={500} height={300} data={data}>
-          <Bar dataKey="Total" fill="#8884d8" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-
-        </BarChart>
-      </div>
- </div>
-
     </div>
   );
 };
